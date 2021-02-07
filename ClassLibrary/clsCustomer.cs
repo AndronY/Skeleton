172da296ea04744fc0ac7e965a41010a52e4b3e3
Andronikos Yiannakou <P2522914@my365.dmu.ac.uk>
2021-02-07 15:16:17 +0000

    Added more tests

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string EmailAddress { get; set; }
+        public bool Above18 { get; set; }
+        public DateTime DateRegistered { get; set; }
     }
 }
--- Testing2/tstCustomer.cs
+++ Testing2/tstCustomer.cs
@@ -13,5 +13,59 @@ namespace Testing2
             clsCustomer ACustomer = new clsCustomer();
             Assert.IsNotNull(ACustomer);
         }
+
+        [TestMethod]
+        public void IDPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Int32 TestData = 1;
+            ACustomer.ID = TestData;
+            Assert.AreEqual(ACustomer.ID, TestData);
+        }
+
+        [TestMethod]
+        public void FullNamePropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            String TestData = "John Green";
+            ACustomer.FullName = TestData;
+            Assert.AreEqual(ACustomer.FullName, TestData);
+        }
+
+        [TestMethod]
+        public void AddressPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            String TestData = "Some Street, Leicester, LE2 8FE";
+            ACustomer.Address = TestData;
+            Assert.AreEqual(ACustomer.Address, TestData);
+        }
+
+        [TestMethod]
+        public void EmailAddressPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            String TestData = "JohnGreen@gmail.com";
+            ACustomer.EmailAddress = TestData;
+            Assert.AreEqual(ACustomer.EmailAddress, TestData);
+        }
+
+        [TestMethod]
+        public void Above18PropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean TestData = true;
+            ACustomer.Above18 = TestData;
+            Assert.AreEqual(ACustomer.Above18, TestData);
+        }
+
+        [TestMethod]
+        public void DateRegisteredPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+            DateTime TestData = DateTime.Now.Date;
+            ACustomer.DateRegistered = TestData;
+            Assert.AreEqual(ACustomer.DateRegistered, TestData);
+        }
     }
 }
