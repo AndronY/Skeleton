046ef66ff141f3f9591c1c7d5368724f18b36b7e
Sameeha Mamodo <p2521112@my365.dmu.ac.uk>
2021-02-09 20:29:24 +0000

    Updated the staff data entry and staff viewer

--- AdminSystem/StaffViewer.aspx.cs
+++ AdminSystem/StaffViewer.aspx.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        clsStaff AStaff = new clsStaff();
+        AStaff = (clsStaff)Session["AStaff"];
+        Response.Write(AStaff.IdNumber);
+        Response.Write(AStaff.Name);
+        Response.Write(AStaff.DateStarted);
+        Response.Write(AStaff.JobRole);
+        Response.Write(AStaff.Email);
+        
     }
 }
