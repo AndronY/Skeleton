9dff41b9e9fa68350925001ad6423fe02e3aaa83
Mohammed Sayeed <P2521933@my365.dmu.ac.uk>
2021-02-14 14:04:11 +0000

    Added Test class for Order and a Order class. My test did not pass as there is an error on other class clsStaff.

---
+++ Testing4/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Testing4/tstOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing4
+{
+    [TestClass]
+    public class TstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            ClsOrder AnOrder = new ClsOrder();
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
