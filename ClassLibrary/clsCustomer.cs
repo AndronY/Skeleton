c38a4ef9752f624af7893c86d2464243b93aee49
Andronikos Yiannakou <P2522914@my365.dmu.ac.uk>
2021-02-15 15:45:05 +0000

    Created the Find Method (Data Layer)

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -4,11 +4,105 @@ namespace ClassLibrary
 {
     public class clsCustomer
     {
-        public int ID { get; set; }
-        public string FullName { get; set; }
-        public string Address { get; set; }
-        public string EmailAddress { get; set; }
-        public bool Above18 { get; set; }
-        public DateTime DateRegistered { get; set; }
+        private Int32 mID;
+        private string mFullName;
+        private string mAddress;
+        private string mEmailAddress;
+        private Boolean mAbove18;
+        private DateTime mDateRegistered;
+
+        public Int32 ID
+        {
+            get
+            {
+                return mID;
+            }
+            set
+            {
+                mID = value;
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return mFullName;
+            }
+            set
+            {
+                mFullName = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return mAddress;
+            }
+            set
+            {
+                mAddress = value;
+            }
+        }
+
+        public string EmailAddress
+        {
+            get
+            {
+                return mEmailAddress;
+            }
+            set
+            {
+                mEmailAddress = value;
+            }
+        }
+
+        public bool Above18
+        {
+            get
+            {
+                return mAbove18;
+            }
+            set
+            {
+                mAbove18 = value;
+            }
+        }
+
+        public DateTime DateRegistered
+        {
+            get
+            {
+                return mDateRegistered;
+            }
+            set
+            {
+                mDateRegistered = value;
+            }
+        }
+
+        public bool Find(int ID)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@ID", ID);
+            DB.Execute("sproc_tblCustomers_FilerByID");
+            if (DB.Count == 1)
+            {
+                mID = Convert.ToInt32(DB.DataTable.Rows[0]["ID"]);
+                mFullName = Convert.ToString(DB.DataTable.Rows[0]["FullName"]);
+                mAddress = Convert.ToString(DB.DataTable.Rows[0]["Address"]);
+                mEmailAddress = Convert.ToString(DB.DataTable.Rows[0]["EmailAddress"]);
+                mAbove18 = Convert.ToBoolean(DB.DataTable.Rows[0]["Above18"]);
+                mDateRegistered = Convert.ToDateTime(DB.DataTable.Rows[0]["DateRegistered"]);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
