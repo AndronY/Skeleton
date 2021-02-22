34ea3a85352308edfa500004647c38fb6fdb01c0
Mohammed Sayeed <P2521933@my365.dmu.ac.uk>
2021-02-22 13:33:06 +0000

    Created the Find method (Middle Layer) and Find method (Data Layer)

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -8,6 +8,7 @@ using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
+
     protected void Page_Load(object sender, EventArgs e)
     {
        
@@ -18,13 +19,13 @@ public partial class _1_DataEntry : System.Web.UI.Page
         //creates a new instance of ClsOrder
         ClsOrder AnOrder = new ClsOrder();
         //captures the OrderID
-        AnOrder.OrderID = int.Parse(txtOrderID.Text);
+        AnOrder.OrderID = Convert.ToInt32(txtOrderID.Text);
         //captures the Order Date
         AnOrder.OrderDate = DateTime.Parse(txtOrderDate.Text);
         //captures the Order Shipped
         AnOrder.OrderShipped = chkOrderShipped.Checked;
         //captures the CustomerID
-        AnOrder.CustomerID = int.Parse(txtCustomerID.Text);
+        AnOrder.CustomerID = Int32.Parse(txtCustomerID.Text);
         //captures the Shipping Address
         AnOrder.ShippingAddress = txtShippingAddress.Text;
         //captures the Order Status
@@ -35,6 +36,9 @@ public partial class _1_DataEntry : System.Web.UI.Page
         Response.Redirect("OrderViewer.aspx");
     }
 
-   
+      
+        }
 
-    }
+
+    
+
