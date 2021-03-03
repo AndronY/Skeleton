89b4dc22efe92a3a59c65a5be4f33f53aa0f783d
Adam Aziz <P2521246@my365.dmu.ac.uk>
2021-03-03 14:28:40 +0000

    Added testing for Middle Layer properties

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,13 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int StockQuantity { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
     }
 }
