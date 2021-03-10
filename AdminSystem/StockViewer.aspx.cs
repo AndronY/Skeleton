d1b0747e0996a19610538b76a4f7b1bae7cf5255
Adam Aziz <P2521246@my365.dmu.ac.uk>
2021-03-10 15:24:28 +0000

    Added Presentation Layer

--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        clsStock Astock = new clsStock();
+        Astock = (clsStock)Session["AStock"];
+        Response.Write(Astock.ProductDescription);
+        Response.Write(Astock.Price);
+        Response.Write(Astock.StockQuantity);
+        Response.Write(Astock.DateAdded);
     }
 }
