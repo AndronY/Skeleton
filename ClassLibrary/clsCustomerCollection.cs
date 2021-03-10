6c45f4b16f9ac99be14d81ee50682c2b850d3361
Andronikos Yiannakou <P2522914@my365.dmu.ac.uk>
2021-03-10 12:20:45 +0000

    Creating the Collection Class Properties (Middle Layer)

---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsCustomerCollection
+    {
+        public clsCustomerCollection()
+        {
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblCustomers_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank customer
+                clsCustomer ACustomer = new clsCustomer();
+                //read in the fields from the current record
+                ACustomer.ID = Convert.ToInt32(DB.DataTable.Rows[Index]["ID"]);
+                ACustomer.FullName = Convert.ToString(DB.DataTable.Rows[Index]["FullName"]);
+                ACustomer.Address = Convert.ToString(DB.DataTable.Rows[Index]["Address"]);
+                ACustomer.EmailAddress = Convert.ToString(DB.DataTable.Rows[Index]["EmailAddress"]);
+                ACustomer.DateRegistered = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateRegistered"]);
+                ACustomer.Above18 = Convert.ToBoolean(DB.DataTable.Rows[Index]["Above18"]);
+                //add the record to the private data member
+                mCustomerList.Add(ACustomer);
+                //point to the next record
+                Index++;
+            }
+        }
+
+
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+
+        //public customer list
+        public List<clsCustomer> CustomerList
+        {
+            get
+            {
+                //return the private data
+                return mCustomerList;
+            }
+            set
+            {
+                //set the private data
+                mCustomerList = value;
+
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mCustomerList.Count;
+            }
+            set
+            {
+              //  
+            }
+        }
+
+        public clsCustomer ThisCustomer { get; set; }
+    }
+}
