b63e7bf87ea474f2ff2ebf9142d01716119f0f53
Mohammed Sayeed <P2521933@my365.dmu.ac.uk>
2021-04-14 00:56:15 +0100

    Added code to the ClsOrdercollection class and created the tstOrderCollection class as well as added code.

---
+++ Testing4/tstOrderCollection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstOrderCollection
+    {
+        [TestMethod]
+        public void InstanceOK();
+        ClsOrderCollection AllOrder = new ClsOrderCollection();
+        Assert.IsNotNull(AllOrder);
+
+         }
+    [TestMethod]
+    public void OrderListOK()
+    {
+        ClsOrderCollection AllOrder = new ClsOrderCollection();
+        List<ClsOrder> TestList = new List<ClsOrder>();
+        ClsOrder TestItem = new ClsOrder();
+        TestItem.OrderID = 1;
+        TestItem.CustomerID = 1;
+        TestItem.ShippingAddress = "Test street";
+        TestItem.OrderStatus = "Dispatched";
+        TestItem.OrderDate = DateTime.Now.Date;
+        TestItem.OrderShipped = true;
+        TestList.Add(TestItem);
+        AllOrder.OrderList = TestList;
+        Assert.AreEqual(AllOrder.OrderList, TestList);
+    }
+    [TestMethod]
+    public void ThisOrderPropertyOK()
+    {
+        ClsOrderCollection AllOrder = new ClsOrderCollection();
+        //Test data to assign to property
+        ClsOrder TestOrder = new ClsOrder();
+        //Set properties of the test item
+        TestItem.OrderID = 1;
+        TestItem.CustomerID = 1;
+        TestItem.ShippingAddress = "Test street";
+        TestItem.OrderStatus = "Dispatched";
+        TestItem.OrderDate = DateTime.Now.Date;
+        TestItem.OrderShipped = true;
+        //Assign data to the property
+        AllOrder.ThisOrder = TestOrder;
+        //Test to see that the two are the same
+        Assert.AreEqual(AllOrder.ThisOrder, TestOrder);
+
+    }
+
+    [TestMethod]
+    public void ListAndCountOK()
+    {
+        ClsOrderCollection AllOrder = new ClsOrderCollection();
+        //Test data to assign to property
+        List<ClsOrder> TestList = new List<ClsOrder>();
+        ClsOrder TestItem = new ClsOrder();
+        //Set properties
+        TestItem.OrderID = 1;
+        TestItem.CustomerID = 1;
+        TestItem.ShippingAddress = "Test street";
+        TestItem.OrderStatus = "Dispatched";
+        TestItem.OrderDate = DateTime.Now.Date;
+        TestItem.OrderShipped = true;
+        TestList.Add(TestItem);
+        //Add item to test list
+        TestList.Add(TestItem);
+        //Assign data to the property
+        AllOrder.OrderList = TestList;
+        //Test to see if the two are the same
+        Assert.AreEqual(AllOrder.Count, TestList.Count);
+    }
+}
