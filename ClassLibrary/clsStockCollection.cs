803925aa97c1ec2877b2b615a00013cda16da877
Adam Aziz <P2521246@my365.dmu.ac.uk>
2021-04-29 17:09:16 +0100

    Stock Collection Testing

---
+++ ClassLibrary/clsStockCollection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsStockCollection
+    {
+        List<clsStock> mStockList = new List<clsStock>();
+        public List<clsStock> StockList
+        {
+            get
+            {
+                return mStockList;
+            }
+            set
+            {
+                mStockList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mStockList.Count;
+            }
+            set
+            {
+                //
+            }
+        }
+        public clsStock ThisStock { get; set; }
+        public clsStockCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblStock_SelectAll");
+            RecordCount = DB.Count;
+            while(Index < RecordCount)
+            {
+                clsStock AStock = new clsStock();
+                AStock.ProductID = Convert.ToInt32(DB.DataTable.Rows[Index]["ProductID"]);
+                AStock.Price = Convert.ToDecimal(DB.DataTable.Rows[Index]["Price"]);
+                AStock.ProductDescription = Convert.ToString(DB.DataTable.Rows[Index]["ProductDescription"]);
+                AStock.StockQuantity = Convert.ToInt32(DB.DataTable.Rows[Index]["StockQuantity"]);
+                AStock.DateListed = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateListed"]);
+                AStock.InStock = Convert.ToBoolean(DB.DataTable.Rows[Index]["InStock"]);
+                mStockList.Add(AStock);
+                Index++;
+            }
+        }
+    }
+}
---
+++ Testing3/tstStockCollection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStockCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStockCollection AllStocks = new clsStockCollection();
+            Assert.IsNotNull(AllStocks);
+        }
+        [TestMethod]
+        public void StockListOk()
+        {
+            clsStockCollection AllStocks = new clsStockCollection();
+            List<clsStock> TestList = new List<clsStock>();
+            clsStock TestItem = new clsStock();
+            TestItem.InStock = true;
+            TestItem.ProductID = 1;
+            TestItem.ProductDescription = "Phone 7";
+            TestItem.DateListed = DateTime.Now.Date;
+            TestItem.StockQuantity = 50;
+            TestItem.Price = 799;
+            TestList.Add(TestItem);
+            AllStocks.StockList = TestList;
+            Assert.AreEqual(AllStocks.StockList, TestList);
+        }
+        [TestMethod]
+        public void ThisAddressPropertyOK()
+        {
+            clsStockCollection AllStocks = new clsStockCollection();
+            clsStock TestStock = new clsStock();
+            TestStock.InStock = true;
+            TestStock.ProductID = 1;
+            TestStock.ProductDescription = "Phone 7";
+            TestStock.DateListed = DateTime.Now.Date;
+            TestStock.StockQuantity = 50;
+            TestStock.Price = 799;
+            AllStocks.ThisStock = TestStock;
+            Assert.AreEqual(AllStocks.ThisStock, TestStock);
+        }
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsStockCollection AllStocks = new clsStockCollection();
+            List<clsStock> TestList = new List<clsStock>();
+            clsStock TestItem = new clsStock();
+            TestItem.InStock = true;
+            TestItem.ProductID = 1;
+            TestItem.ProductDescription = "Phone 7";
+            TestItem.DateListed = DateTime.Now.Date;
+            TestItem.StockQuantity = 50;
+            TestItem.Price = 799;
+            TestList.Add(TestItem);
+            AllStocks.StockList = TestList;
+            Assert.AreEqual(AllStocks.Count, TestList.Count);
+        }
+
+    }
+}
